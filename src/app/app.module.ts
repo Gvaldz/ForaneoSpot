00a357451821c3243266bd33c2c3ce61dc3928a0
Gvaldz <233415@ids.upchiapas.edu.mx>
2024-11-07 16:52:51 -0600

    add: Se crea la vista de inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardInicioComponent } from './shared/dashboard-inicio/dashboard-inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardInicioComponent
   ],
   imports: [
     BrowserModule,
