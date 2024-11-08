a7fe7c269fa08408d2fd2d9dbb7652581a670bb2
vhag233392 <233392@ids.upchiapas.edu.mx>
2024-11-07 22:51:25 -0600

    Login listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {ComidaHomeComponent} from './servicios/comida-home/comida-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  { path: 'comida', component: ComidaHomeComponent },
+  { path: '', redirectTo: '/comida', pathMatch: 'full' },
+  { path: '**', redirectTo: '/comida'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ComidaHomeComponent} from './servicios/comida-home/comida-home.component';
 import {OrdenesComidaComponent} from './shared/ordenes-comida/ordenes-comida.component';
+import { LoginComponent } from './login/login.component';
+import {ReactiveFormsModule} from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
     CardComidaComponent,
     ComidaHomeComponent,
     OrdenesComidaComponent,
+    LoginComponent,
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+
   ],
   providers: [
     provideClientHydration()
