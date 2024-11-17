903636483d476ad05c24fdf360f3aca8337bd77d
vhag233392 <233392@ids.upchiapas.edu.mx>
2024-11-16 23:53:39 -0600

    Vista Modal de Especificaciones en Ordenar Comida

--- src/app/shared/card-comida/card-comida.component.ts
+++ src/app/shared/card-comida/card-comida.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input} from '@angular/core';
 import {Router} from '@angular/router';
+import 'sweetalert2/src/sweetalert2.scss';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-card-comida',
   templateUrl: './card-comida.component.html',
   styleUrl: './card-comida.component.css'
 })
+
+
 export class CardComidaComponent{
 
   constructor(private router: Router,) {
@@ -15,5 +19,20 @@ export class CardComidaComponent{
 
   navegarMenuComida() {
     console.log(`Ordenar comida: ${this.comida.nombre}`);
+    Swal.fire({
+      title: "Especificaciones",
+      input: "text",
+      inputPlaceholder:`Especificaciones para ${this.comida.nombre}`,
+      inputAttributes: {
+        autocapitalize: "off"
+      },
+      showCancelButton: true,
+      confirmButtonText: "ACEPTAR",
+      confirmButtonColor:"blue",
+      cancelButtonText:"CANCELAR",
+      cancelButtonColor:'red',
+      showLoaderOnConfirm: true,
+
+      });
   }
 }
